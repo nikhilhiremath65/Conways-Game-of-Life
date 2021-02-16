3024ebb5be88fbde2d7a433b22cb6f7479c48dfb
Nikhil Hiremath <nikhilhiremath65@gmail.com>
2021-02-16 00:46:20 -0700

    US4 Task31: Created a MenuListener to capture all the menu events

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,18 +1,17 @@
 import java.awt.Toolkit;
 import javax.swing.JMenu;
-
-import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
-public class Canvas extends JFrame {
+public class Canvas extends JFrame implements MenuListener {
 
-	private Thread game;
 	private JMenuBar menu_bar;
 	private Board game_board;
-	private static final long serialVersionUID = 1L;	
-	private JMenu start_button, stop_button;
+	private static final long serialVersionUID = 1L;
+	private JMenu start_button, stop_button, reset_button;
 
 
 	public Canvas() {
@@ -40,7 +39,6 @@ public class Canvas extends JFrame {
 		main_frame.setSize(CommonConstants.DEFAULT_WINDOW_SIZE);
 		main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main_frame.setMinimumSize(CommonConstants.MINIMUM_WINDOW_SIZE);
-		main_frame.getContentPane().setBackground(Color.BLUE);
 
 		Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize();
 		main_frame.setLocation(screen_size.width - main_frame.getWidth()/2, 
@@ -48,18 +46,26 @@ public class Canvas extends JFrame {
 
 		main_frame.setVisible(true);
 	}
+	
+	@Override
+	public void menuSelected(MenuEvent e) {
+		if (e.getSource().equals(reset_button)) {		
+		} 
+		else if (e.getSource().equals(start_button)) {
+			
+		} 
+		else if (e.getSource().equals(stop_button)) {
+		
+		} 	
+	}
 
 
-	public void startGame(boolean start_flag) {
-		if (start_flag) {
-			start_button.setEnabled(false);
-			stop_button.setEnabled(true);
-			game = new Thread(game_board);
-			game.start();
-		} else {
-			start_button.setEnabled(true);
-			stop_button.setEnabled(false);
-			game.interrupt();
-		}
+	@Override
+	public void menuDeselected(MenuEvent e) {
+	}
+
+
+	@Override
+	public void menuCanceled(MenuEvent e) {
 	}
 }
